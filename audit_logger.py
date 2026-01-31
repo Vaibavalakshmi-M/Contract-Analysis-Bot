477edc98d56ad4f3b7bc513500e79b5e3e91644e
Vaibavalakshmi M <163707509+Vaibavalakshmi-M@users.noreply.github.com>
2026-01-31 12:09:50 +0530

    Update audit_logger.py

--- audit_logger.py
+++ audit_logger.py
@@ -1,12 +1,18 @@
 import json
+import os
 from datetime import datetime
 
 def save_audit(file_name, risk_score):
+    log_folder = "audit_logs"
+    os.makedirs(log_folder, exist_ok=True)
+
     log = {
         "file": file_name,
         "timestamp": str(datetime.now()),
         "risk": risk_score
     }
 
-    with open("audit_logs/audit.json", "a") as f:
+    file_path = os.path.join(log_folder, "audit.json")
+
+    with open(file_path, "a") as f:
         f.write(json.dumps(log) + "\n")
